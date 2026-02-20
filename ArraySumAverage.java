1326bc46b49ac3ddb4a94f979492ed80cdef9715
Amaan Khan <mailtoamaandev@gmail.com>
2026-02-20 14:45:15 +0530

    Create ArraySumAverage.java

---
+++ ArraySumAverage.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class ArraySumAverage {
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Welcome to the Program of Array Sum & Average");
+
+		System.out.print("Enter Size of an Array :   ");
+		int size = input.nextInt();
+		int [] numArray = new int[size];
+		int i = 0;
+		while (i < size) {
+			System.out.print("pls enter element no" + (i + 1) + ": ");
+
+			numArray[i] = input.nextInt();
+			i++;
+		}
+		System.out.println("The sum of Array is: " + sum(numArray));
+		System.out.println("The Average of Array is: " + average(numArray));
+		input.close();
+	}
+
+	public static long sum(int[] numArray) {
+		int 	sum = 0;
+		int 	i = 0;
+		while (i < numArray.length) {
+			sum = sum + numArray[i];
+			i++;
+		}
+
+		return sum;
+	}
+
+	public static int average(int[] numArray) {
+		long sum = sum(numArray);
+
+		return (int)(sum / numArray.length);
+
+	}
+
+}
