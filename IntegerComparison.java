fe96ad7f9f7b1faab760dda8363a939fd08abf79
Amaan Khan <mailtoamaandev@gmail.com>
2026-02-21 16:53:16 +0530

    Create IntegerComparison.java

---
+++ IntegerComparison.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class IntegerComparison {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int number1;
+        int number2;
+
+        System.out.print("Input first integer: ");
+        number1 = input.nextInt();
+
+        System.out.print("Input second integer: ");
+        number2 = input.nextInt();
+
+        if (number1 == number2)
+            System.out.printf("%d == %d%n", number1, number2);
+
+        if (number1 != number2)
+            System.out.printf("%d != %d%n", number1, number2);
+
+        if (number1 < number2)
+            System.out.printf("%d < %d%n", number1, number2);
+
+        if (number1 > number2)
+            System.out.printf("%d > %d%n", number1, number2);
+
+        if (number1 <= number2)
+            System.out.printf("%d <= %d%n", number1, number2);
+
+        if (number1 >= number2)
+            System.out.printf("%d >= %d%n", number1, number2);
+
+        input.close();
+    }
+}
