fed988dfdc4fc101e482cb6f7f21930bfc8035f0
Amaan Khan <mailtoamaandev@gmail.com>
2026-02-23 16:31:47 +0530

    Create UserInput.java

---
+++ UserInput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class UserInput {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please Enter your Good Name: ");
+		String name = input.nextLine();
+		System.out.println("Welcome to the java Language " + name);
+	}
+}
